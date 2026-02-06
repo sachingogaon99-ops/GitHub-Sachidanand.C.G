3a6f82b666bc96e132aa507a9e81ce095be42e17
sachingogaon99-ops <sachingogaon99@gmail.com>
2026-02-06 18:17:09 +0530

    inital commit

---
+++ ArithmeticOperation.py
@@ -0,0 +1,11 @@
+n1=10
+n2=2
+
+
+print("Printing Answera")
+print("sum:",n1+n2)
+print("difference:",n1-n2)
+print("multiplication:",n1*n2)
+print("Division:",n1/n2)
+print("modulo:",n1%n2)
+print("power:",n1**n2)
---
+++ personalDetails.py
@@ -0,0 +1,12 @@
+Name="Sachidanand.C.G"
+age=19
+date_of_birth="19-09-2006"
+college_name="P.C.Jabin"
+address="Vidyanagar-Hubli"
+
+print("Printing Personal Details**")
+print(f"Name:",Name)
+print(f"age:",age)
+print(f"DOB:",date_of_birth)
+print(f"CollegeName:",college_name)
+print(f"Address:",address)
